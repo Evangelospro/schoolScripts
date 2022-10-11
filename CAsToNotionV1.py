baa8564c69eda8a39e5a3e3b10d7beeadde0a1fa
Evangelospro <elioudakis123@gmail.com>
2022-10-11 22:10:54 +0300

    finished ca to notion, weduc remaining

--- CAsToNotionV1.py
+++ CAsToNotionV1.py
@@ -1,49 +1,89 @@
-# /usr/bin/python3.9
+#!/usr/bin/python3
 
 import requests
 import datetime
-import math
 import pyfiglet
 import subprocess as s
 import json
 import pandas as pd
 import convertapi
 
+startTime = datetime.datetime.now()
+
 ascii_art = pyfiglet.figlet_format("CAs To Notion\nV 1.0")
 print(ascii_art)
-print(f"Current Time: {datetime.datetime.now().time()}")
+print(f"Current Time: {startTime.time()}")
 
 # Variables:
 ca_url = "https://exams.englishschool.ac.cy"
 login_url = f"{ca_url}/login_check"
 export_url = f"{ca_url}/student/assessments/export"
-print(login_url)
+notion_api_pages = "https://api.notion.com/v1/pages"
+notion_api_databases = "https://api.notion.com/v1/databases"
+
 creds = open("creds.json", "r").read()
 creds = json.loads(creds)
 password = creds["password"]
 username = creds["username"]
 token = creds['notion_token']
 convertapi.api_secret = creds["convertapi_token"]
-already_added_CAs_titles = []
-startTime = datetime.datetime.now()
-notion_api_pages = "https://api.notion.com/v1/pages"
-notion_api_databases = "https://api.notion.com/v1/databases"
-ca_db_id = "be3ffad9fb4247eb8f0f5704c8b7835a"
+ca_db_id = creds['ca_db_id']
+
 headers = {
     'Authorization': f"Bearer {token}",
     'Content-Type': 'application/json',
     'Notion-Version': '2021-08-16'
 }
-already_added_CAs = requests.post(f"{notion_api_databases}/{ca_db_id}/query", headers=headers)
-already_added_CAs = already_added_CAs.json()
 
-# Initialize
+def alreadyAdded(title, date):
+    already_added_CAs = requests.post(f"{notion_api_databases}/{ca_db_id}/query", headers=headers)
+    already_added_CAs = already_added_CAs.json()['results']
+    for already_added_CA_title in already_added_CAs:
+        alreadyAddedTitle = already_added_CA_title['properties']['Title']['title'][0]['text']['content']
 
+        alreadyAddedDate = already_added_CA_title['properties']['Day scheduled']['date']['start']
+        if title == alreadyAddedTitle and date == alreadyAddedDate:
+            return True
+    return False
 
 def notify(notification):
     print(f"\n{notification}")
     return s.call(['notify-send', 'ES CA Tool', notification])
 
+
+def postCA(title, subject, week, date):
+    data = {
+        "parent": {"database_id": ca_db_id},
+        "properties": {
+            "Title": {
+                "title": [
+                    {
+                        "text": {
+                            "content": title
+                        }
+                    }
+                ]
+            },
+            "Subject": {
+                "select": {
+                    "name": subject
+                }
+            },
+            "Day scheduled": {
+                "date": {
+                    "start": date,
+                    # "remind": "19"
+                }
+            },
+            "Week": {
+                "select": {
+                        "name":  week
+                    }
+            },
+        }
+    }
+    response = requests.post(notion_api_pages, headers=headers, json=data)
+    print(response.json())
 
 def main():
     s = requests.Session()
@@ -62,43 +102,15 @@ def main():
         title = row['Common Assessment Schedule']
         week = row['Unnamed: 2']
         date = row['Unnamed: 3']
-        print(type(title), title)
-        if type(subject) == str and type(title) == str and type(week) == str and type(date) == str:
-            count +=1
-            print(f"Subject: {subject} | Title: {title} | Week: {week} | Date: {date}")
-            CAs_to_post = {
-                "parent": {"database_id": ca_db_id},
-                "properties": {
-                    "Title": {
-                        "title": [
-                            {
-                                "text": {
-                                    "content": title
-                                }
-                            }
-                        ]
-                    },
-                    "Subject": {
-                        "select": {
-                            "name": subject
-                        }
-                    },
-                    "Day scheduled": {
-                        "date": {
-                            "start": date,
-                            # "remind": "19"
-                        }
-                    },
-                    "Week": {
-                        "select": {
-                                "name":  week
-                            }
-                    },
-                }
-            }
-            print(CAs_to_post)
-            response = requests.post(notion_api_pages, headers=headers, json=CAs_to_post)
-            print(response.json())
+        if alreadyAdded(title, date):
+            print(f"Already added {title} on {date}")
+        elif type(subject) == str and subject != "Subject" and type(title) == str and type(week) == str and type(date) == str :
+            if(date == "Still pending day allocation"):
+                print(f"CA {title} is still pending day allocation")
+            else:
+                count +=1
+                print(f"Subject: {subject} | Title: {title} | Week: {week} | Date: {date}")
+                postCA(title, subject, week, date)
     notification = f"Done I added succesfully {count} Common Assesments to your Notion CA page!\nSo you can be organized!!!\nNow go start studying, Common Assesments are easy but you have to study for something in your life!!!\nTime taken to run is: {datetime.datetime.now() - startTime}"
     notify(notification)
 
